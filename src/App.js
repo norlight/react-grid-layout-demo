82d61f03dd0f969b71eada6368eb452feaeb1cc4
Shaw <norlights@163.com>
2018-07-10 19:41:23 +0800

    * responsive UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
-import GridLayout from 'react-grid-layout'
-import { Responsive as ResponsiveGridLayout } from 'react-grid-layout'
+import { Responsive, WidthProvider } from 'react-grid-layout'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 import PostList from './PostList'
 
+const ResponsiveReactGridLayout = WidthProvider(Responsive)
+ResponsiveReactGridLayout.defaultProps = {
+    className: 'layout',
+    rowHeight: 30,
+    onLayoutChange: function(){},
+
+    //只针对大中小三个屏幕尺寸进行断点，粗略对应电脑、平板、手机
+    //breakpoints: {lg: 1200, md:900, sm:400},
+    cols: {lg: 12, md: 9, sm: 3},
+}
+
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            layouts: {
+                lg: largeLayout(),
+                md: mediumLyaout(),
+                sm: smallLayout()
+            }
+        }
+    }
     render() {
         const boxStyle = {
             borderRadius: '5px',
@@ -15,40 +36,154 @@ class App extends Component {
         }
 
         return (
-            <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+            <ResponsiveReactGridLayout
+                layouts={this.state.layouts}
+            >
                 <div key='list'
-                    data-grid={{ x: 0, y: 0, w: 3, h: 2, static: true }}
+                    className='static'
                 >
                     <PostList></PostList>
                 </div>
                 <div key='detail'
-                    data-grid={{ x: 3, y: 0, w: 6, h: 12}}
                     style={{ ...boxStyle, border: '3px solid #FF4949' }}
                 >
                     <PostDetail></PostDetail>
                 </div>
                 <div key='note'
-                    data-grid={{ x: 9, y: 0, w: 3, h: 6 }}
                     style={{ ...boxStyle, border: '3px solid #F7BA2A' }}
                 >
                     <PostNoteList></PostNoteList>
                 </div>
                 <div key='discussion'
-                    data-grid={{ x: 9, y: 0, w: 3, h: 6 }}
                     style={{ ...boxStyle, border: '3px solid #20A0FF' }}
                 >
                     <PostDiscussionList></PostDiscussionList>
                 </div>
-            </GridLayout>
+            </ResponsiveReactGridLayout>
         )
     }
+
 }
+
+//三列，列表-左，详情-中，笔记-右上，讨论-右下
+function largeLayout() {
+    return [
+        {
+            i: 'list',
+            static: true,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 9
+        },
+        {
+            i: 'detail',
+            static: false,
+            x: 3,
+            y: 0,
+            w: 6,
+            h: 12
+        },
+        {
+            i: 'note',
+            static: false,
+            x: 9,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+        {
+            i: 'discussion',
+            static: false,
+            x: 9,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+    ]
+}
+
+//两列，列表-左，详情-右上，笔记-右下左，讨论-右下右
+function mediumLyaout() {
+    return [
+        {
+            i: 'list',
+            static: true,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 9
+        },
+        {
+            i: 'detail',
+            static: false,
+            x: 3,
+            y: 0,
+            w: 6,
+            h: 12
+        },
+        {
+            i: 'note',
+            static: false,
+            x: 3,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+        {
+            i: 'discussion',
+            static: false,
+            x: 6,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+    ]
+}
+
+//单列，列表-详情-笔记-讨论
+function smallLayout() {
+    return [
+        {
+            i: 'list',
+            static: true,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 9
+        },
+        {
+            i: 'detail',
+            static: false,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 12
+        },
+        {
+            i: 'note',
+            static: false,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+        {
+            i: 'discussion',
+            static: false,
+            x: 0,
+            y: 0,
+            w: 3,
+            h: 6
+        },
+    ]
+}
+
 
 function PostDetail() {
     return (
         <div>
-            <p>
-                <h1>文章标题</h1>
+            <h1>文章标题</h1>
                 文章详情<br /> 
                 文章详情<br /> 
                 文章详情<br />
@@ -66,8 +201,6 @@ function PostDetail() {
                 文章详情<br />
                 文章详情<br />
                 文章详情<br />
-            </p>
-        
         </div>
     )
 
